af80f7379286ecae6f854fed93a1cd0c179d6738
Pedro Henrique <pedrohenrique1603@gmail.com>
2024-04-11 09:28:23 -0300

    CRUD Classificação iniciado

---
+++ controller/controller_diretor.js
@@ -0,0 +1,42 @@
+/********************************
+ * Objetivo: Arquivo responsável pela interação entre o APP e a MODEL, que teremos todas
+ * as tratativas e regra de negócio para o CRUD de Filmes
+ * Data: 30/01/2024
+ * Autor: Pedro Pedraga
+ * Versão: 1.0
+ *******************************/
+
+ // Import do arquivo de configuração do projeto
+const message = require('../modulo/config.js')
+
+// Import do arquivo DAO para manipular dados do banco de dados
+const diretoresDAO = require('../model/DAO/diretores.js');
+
+
+const getListarDiretores = async function(){
+    let listaDiretores;
+    
+    let diretoresJSON = {};
+
+    if(listaDiretores){
+        return listaDiretores
+    } else{
+        let dadosDiretores = await diretoresDAO.selectAllDirectors();
+            // Verifica se existem dados retornados do DAO
+    if(dadosDiretores){
+        if(dadosDiretores.length > 0){
+        // Montando a estrutura do JSOm
+        diretoresJSON.diretores = dadosDiretores;
+        diretoresJSON.quantidade = dadosDiretores.length;
+        diretoresJSON.status_code = 200;
+        // Retorna o JSON montado
+        return diretoresJSON; // 200
+        }else{
+            return message.ERROR_NOT_FOUND // 404
+        }
+        } else{
+            return message.ERROR_INTERNAL_SERVER_DB // 500
+
+    }
+    }
+}
---
+++ model/DAO/diretores.js
@@ -0,0 +1,37 @@
+/********************************
+ * Objetivo: Cria a interação com o Banco de dados MySQL para fazer o CRUD de Diretores
+ * Data: 09/04/2024
+ * Autor: Pedro Pedraga
+ * Versão: 1.0
+ *******************************/
+
+// Import da biblioteca do prisma client
+const { PrismaClient } = require ('@prisma/client')
+
+// Instaciando o o bjeto prisma com as caracteristicas do prisma client
+const prisma = new PrismaClient();
+
+
+const selectAllDirectors = async function(){
+
+    // Script sql para listar todos os registros
+    let sql = 'select * from tbl_diretores order by id desc';
+
+    // $queryRawUnsafe(sql)  = Encaminha apenas a variável
+    // $queryRaw('select * from tbl_diretores) = Encaminha o script do banco 
+
+    // Executa o script no banco de dados e recebe o retorno dos dados da variavel rsAtores
+    let rsDiretores = await prisma.$queryRawUnsafe(sql)
+     // Para usar await a função necessita ser async(async function)
+
+    // Tratamento de erro para retornar dados ou retornar false
+     if(rsDiretores.length > 0)
+     return rsDiretores;
+     else
+        return false
+
+    }
+
+module.exports = {
+    selectAllDirectors
+}
